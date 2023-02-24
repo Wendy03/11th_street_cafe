2250766d3959a1e04f81b2c3211ee2c18c47fc42
wendy03 <wendysu03@gmail.com>
2023-02-25 00:49:58 +0800

    add modal

---
+++ src/mixins/modalMixin.js
@@ -0,0 +1,15 @@
+import Modal from 'bootstrap/js/dist/modal';
+
+export default {
+  methods: {
+    showModal() {
+      this.modal.show();
+    },
+    hideModal() {
+      this.modal.hide();
+    },
+  },
+  mounted() {
+    this.modal = new Modal(this.$refs.modal);
+  },
+};
