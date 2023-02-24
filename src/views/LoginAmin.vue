c8a8fcd8b2d12a4e3e842335933734bd89226692
wendy03 <wendysu03@gmail.com>
2023-02-25 00:47:44 +0800

    add login page

---
+++ src/views/LoginAmin.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="mt-5">
+    <div class="row justify-content-center">
+      <h1 class="h3 mb-3 font-weight-normal text-center">請先登入</h1>
+      <LoginForm></LoginForm>
+    </div>
+    <p class="mt-5 mb-3 text-muted text-center">&copy; 2021~∞ - 六角學院</p>
+  </div>
+</template>
+
+<script>
+import LoginForm from '@/components/backend/LoginForm.vue';
+
+export default {
+  data() {
+    return {};
+  },
+  components: { LoginForm },
+};
+</script>
