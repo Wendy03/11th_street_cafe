79002709882724b0957b0f6d7e9ee7af4fcb7201
wendy03 <wendysu03@gmail.com>
2023-02-25 02:10:06 +0800

    deploy

--- assets/statusStore-0ee6f998.js
+++
@@ -1,1 +0,0 @@
-import{g as s}from"./index-d337e7a9.js";const o=s("loadingStore",{state:()=>({loadingItem:"",isProcessing:!1,isLoading:!1})});export{o as s};
---
+++ assets/statusStore-add7e598.js
@@ -0,0 +1,1 @@
+import{g as s}from"./index-0c942f24.js";const o=s("loadingStore",{state:()=>({loadingItem:"",isProcessing:!1,isLoading:!1})});export{o as s};
