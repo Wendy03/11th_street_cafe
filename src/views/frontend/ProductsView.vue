f71d63f640eb0fda1bffe7bccfa38cb369b3ad3c
wendy03 <wendysu03@gmail.com>
2023-02-25 00:50:40 +0800

    add product pages

---
+++ src/views/frontend/ProductsView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container my-9">
+    <VueLoading v-model:active="isLoading"></VueLoading>
+    <div class="row">
+      <ProductCategory/>
+      <ProductList/>
+    </div>
+  </div>
+</template>
+
+<script>
+import ProductCategory from '@/components/frontend/ProductCategory.vue';
+import ProductList from '@/components/frontend/ProductList.vue';
+import statusStore from '@/stores/statusStore';
+import { mapState } from 'pinia';
+
+export default {
+  data() {
+    return {};
+  },
+  components: { ProductCategory, ProductList },
+  computed: {
+    ...mapState(statusStore, ['isLoading']),
+  },
+};
+</script>
